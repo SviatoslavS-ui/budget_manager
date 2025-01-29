43ab8ddfa2e9210ac54233d776e6ef631ccccf8c
Sviat Shu <svyatsh@gmail.com>
2025-01-29 17:18:52 +0000

    project started

---
+++ budget_manager/Models/ExpenseDBContext.cs
@@ -0,0 +1,23 @@
+﻿using budget_manager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        private string connection_string = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connection_string);
+            }
+        }
+    }
+}
